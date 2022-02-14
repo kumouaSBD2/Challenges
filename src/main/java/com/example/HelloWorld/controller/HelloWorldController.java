781cef55239d85b42b6b2a6caaa56ae7cdb498b9
kumouaSBD2 <ku.moua@sbd2.com>
2022-02-14 11:05:09 -0500

    Added an optional request variable to hello()

--- src/main/java/com/example/HelloWorld/controller/HelloWorldController.java
+++ src/main/java/com/example/HelloWorld/controller/HelloWorldController.java
@@ -2,15 +2,26 @@ package com.example.HelloWorld.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
-@RequestMapping("/api")
+@RequestMapping
 public class HelloWorldController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello, world!";
+    public String hello(@RequestParam(name = "shout", required = false) boolean shout) {
+
+        String message = "Hello, world!";
+
+        if(shout == true){
+            return message.toUpperCase(Locale.ROOT);
+        } else{
+            return message;
+        }
+
     }
 
 }
